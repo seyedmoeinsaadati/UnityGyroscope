34eb0b6b4520237b6e908d965470c9f278a8f6c3
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-07-07 18:48:41 +0330

    Add TripleAxis.unity

---
+++ gyroscope/Assets/Scripts/GyroAttitude.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroAttitude : MonoBehaviour
+{
+    public Vector3 weightAxis = Vector3.one;
+
+    private Vector3 rotationValue;
+
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    void Update()
+    {
+        rotationValue = Input.gyro.attitude.eulerAngles;
+
+        rotationValue.x *= weightAxis.x;
+        rotationValue.y *= weightAxis.y;
+        rotationValue.z *= weightAxis.z;
+
+        transform.localRotation = Quaternion.Euler(rotationValue);
+    }
+}
