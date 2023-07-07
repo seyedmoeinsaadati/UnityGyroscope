1b0eaafe485ed7ca7ae3f2cafcb5ec60dc4b076e
Seyed Moein Saadati <saadati.moin@gmail.com>
2023-07-07 17:30:33 +0330

    Add SphereMovement.cs (use rotationRate)

---
+++ gyroscope/Assets/Scripts/GyroInput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroInput : MonoBehaviour
+{
+
+
+    public static bool IsActive
+    {
+        get => Input.gyro.enabled;
+        set => Input.gyro.enabled = value;
+    }
+
+
+
+}
---
+++ gyroscope/Assets/Scripts/SphereMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereMovement : MonoBehaviour
+{
+    public bool biased;
+    public float rotationSpeed = 1;
+
+    void Start()
+    {
+        GyroInput.IsActive = true;
+    }
+
+    void Update()
+    {
+        if (biased)
+        {
+            transform.Rotate(0, Time.deltaTime * rotationSpeed * (-Input.gyro.rotationRateUnbiased.y), 0);
+        }
+        else
+        {
+            transform.Rotate(0, Time.deltaTime * rotationSpeed * (-Input.gyro.rotationRateUnbiased.y), 0);
+        }
+
+    }
+}
